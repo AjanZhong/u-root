9cdc9245622f08b58c95c608f7111402bcc0652f
Ajan Zhong <ajan.zhong@newfw.com>
2024-10-15 13:16:39 +0800

    refine arm64

--- pkg/boot/universalpayload/utilities_arch_arm64.go
+++ pkg/boot/universalpayload/utilities_arch_arm64.go
@@ -6,7 +6,13 @@ package universalpayload
 
 import (
 	"encoding/binary"
+	"reflect"
+	"unsafe"
 )
+
+func addrOfStart() uintptr
+func addrOfStackTop() uintptr
+func addrOfHobAddr() uintptr
 
 func getPhysicalAddressSizes() (uint8, error) {
 	// Return hardcode for arm64
@@ -14,51 +20,53 @@ func getPhysicalAddressSizes() (uint8, error) {
 	return 44, nil
 }
 
-// Due to lack of feature support in Golang assembly AARCH64 architecture,
-// for instance lack of directive 'NOSPLIT', we need plenty of workaround
-// to implement following opcode to assembly and construct trampoline
-// in buffer. We are using opcode directly for current stage.
-//
 // Constrcut trampoline code before jump to entry point of FIT image.
-// Due to lack of support to set value of register x0 in kexec,
+// Due to lack of support to set value of Registers in kexec,
 // bootloader parameter needs to be prepared in trampoline code.
-// Also stack is prepared in trampoline code snippet to ensure no data leak,
-// and to ensure 64-bit entry code can use stack directly.
-//
-// Trampoline code snippet is prepared as following:
-//
-//	buf[0 - 3]   : 0x580000c4 - ldr x4, #0x18 (PC relative: buf[24 - 31])
-//	buf[4 - 7]   : 0x580000e0 - ldr x0, #0x1c (PC relative: buf[32 - 39])
-//	buf[8 - 11]  : 0xaa1f03e1 - mov x1, xzr
-//	buf[12 - 15] : 0x580000e2 - ldr x2, #0x1c (PC relative: buf[40 - 47])
-//	buf[16 - 19] : 0x9100005f - mov sp, x2
-//	buf[20 - 23] : 0xd61f0080 - br  x4
-//	buf[24 - 27] : uint32(uint64(entry)&0xffffffff))
-//	buf[28 - 31] : uint32(uint64(entry)>>32))
-//	buf[32 - 35] : uint32(uint64(hobAddr)&0xffffffff))
-//	buf[36 - 39] : uint32(uint64(hobAddr)>>32))
-//	buf[40 - 43] : uint32(uint64(stackTop)&0xffffffff))
-//	buf[44 - 47] : uint32(uint64(stackTop)>>32))
+// Also stack is prepared in trampoline code snippet to ensure no data leak.
 func constructTrampoline(buf []uint8, hobAddr uint64, entry uint64) []uint8 {
-	appendUint32 := func(slice []uint8, value uint32) []uint8 {
-		tmpBytes := make([]uint8, 4)
-		binary.LittleEndian.PutUint32(tmpBytes, value)
+	ptrToSlice := func(ptr uintptr, size int) []byte {
+		var data []byte
+
+		sh := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+		sh.Data = ptr
+		sh.Len = size
+		sh.Cap = size
+
+		return data
+	}
+
+	trampBegin := addrOfStart()
+	trampStack := addrOfStackTop()
+	trampHob := addrOfHobAddr()
+
+	tramp := ptrToSlice(trampBegin, int(trampStack-trampBegin))
+
+	buf = append(buf, tramp...)
+
+	padWithLength := func(slice []uint8, len uint64) []uint8 {
+		tmpBytes := make([]uint8, len)
 		return append(slice, tmpBytes...)
 	}
 
+	stackOffset := trampStack & 0xFFFF
+	gapLen := stackOffset - (trampStack - trampBegin)
+	buf = padWithLength(buf, uint64(gapLen))
+
 	stackTop := hobAddr + tmpStackTop
-	buf = appendUint32(buf, 0x580000c4)
-	buf = appendUint32(buf, 0x580000e0)
-	buf = appendUint32(buf, 0xaa1f03e1)
-	buf = appendUint32(buf, 0x580000e2)
-	buf = appendUint32(buf, 0x9100005f)
-	buf = appendUint32(buf, 0xd61f0080)
-	buf = appendUint32(buf, uint32(uint64(entry)&0xffffffff))
-	buf = appendUint32(buf, uint32(uint64(entry)>>32))
-	buf = appendUint32(buf, uint32(uint64(hobAddr)&0xffffffff))
-	buf = appendUint32(buf, uint32(uint64(hobAddr)>>32))
-	buf = appendUint32(buf, uint32(uint64(stackTop)&0xffffffff))
-	buf = appendUint32(buf, uint32(uint64(stackTop)>>32))
+	appendUint64 := func(slice []uint8, value uint64) []uint8 {
+		tmpBytes := make([]uint8, 8)
+		binary.LittleEndian.PutUint64(tmpBytes, value)
+		return append(slice, tmpBytes...)
+	}
+
+	padLen := uint64(trampHob - trampStack - 8)
+
+	buf = appendUint64(buf, stackTop)
+	buf = padWithLength(buf, padLen)
+	buf = appendUint64(buf, hobAddr)
+	buf = padWithLength(buf, padLen)
+	buf = appendUint64(buf, entry)
 
 	return buf
 }
